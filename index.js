a6bdc4339eb6ae7bcc607c448539ce287fffe7df
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-05-02 18:30:52 +0600

    json updated and dynamic recipe added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 const port =process.env.PORT || 5000;
 
 const recipe= require('./data/recipe.json');
 const category = require('./data/category.json')
 
+app.use(cors())
 app.get('/',(req,res) =>{
     res.send('Cooking in running')
 })
@@ -15,6 +16,13 @@ app.get('/recipe',(req,res) =>{
 app.get('/category',(req, res)=>{
     res.send(category)
 })
+app.get('/recipe/:id',(req, res) =>{
+    const id = parseInt(req.params.id);
+    console.log(id)
+    const selectedChef = recipe.filter(n => parseInt(n.category__id) === id)
+    res.send(selectedChef)
+})
+
 
 app.listen(port , () =>{
     console.log(`Cooking is continue at port: ${port}`)
